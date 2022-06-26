b11519fd0fe8b3bdd34451144523ab5ef9a668d0
Péter Markó <petermarko@outlook.hu>
2022-06-26 16:15:47 +0200

    Add Swagger

--- ContactsApi/Program.cs
+++ ContactsApi/Program.cs
@@ -1,12 +1,33 @@
+using Microsoft.OpenApi.Models;
 using MiniValidation;
 using System.ComponentModel.DataAnnotations;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Contacts API",
+        Description = "A .NET 6 Minimal API Demo"
+    });
+});
 builder.Services.AddScoped<IContactService, ContactService>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapPost("/contact", (ContactPerson contactPerson, IContactService contactService)
     => ! MiniValidator.TryValidate(contactPerson, out var errors)
