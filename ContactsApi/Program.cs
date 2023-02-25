97438ec68c37866b9f207e761268134211deaaeb
Péter Markó <petermarko@outlook.hu>
2023-02-25 18:01:59 +0100

    Added NLog with Seq target.

--- ContactsApi/Program.cs
+++ ContactsApi/Program.cs
@@ -1,12 +1,17 @@
+using Contracts;
+using LoggerService;
 using Microsoft.OpenApi.Models;
 using MiniValidation;
+using NLog;
 using System.ComponentModel.DataAnnotations;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => 
+builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
@@ -16,29 +21,48 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 builder.Services.AddScoped<IContactService, ContactService>();
+builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options => 
+    app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         options.RoutePrefix = string.Empty;
     });
 }
 
-app.MapPost("/contact", (ContactPerson contactPerson, IContactService contactService)
-    => ! MiniValidator.TryValidate(contactPerson, out var errors)
-    ? Results.ValidationProblem(errors)
-    : Results.Ok(contactService.Create(contactPerson)));
+app.MapPost("/contact", (ContactPerson contactPerson, IContactService contactService, ILoggerManager logger) =>
+    {
+        var isValid = MiniValidator.TryValidate(contactPerson, out var errors);
+        if (!isValid)
+        {
+            foreach (var entry in errors)
+            {
+                foreach (var error in entry.Value)
+                {
+                    logger.LogError($"Error while creating new contact: {error}");
+                }
+            }
+            
+            return Results.ValidationProblem(errors); 
+        }
+
+        var contact = contactService.Create(contactPerson);
+
+        logger.LogInfo($"New contact created: {contactPerson.FirstName} {contactPerson.LastName}");
+
+        return Results.Ok(contact);
+    });
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
 
-public partial class Program {}
+public partial class Program { }
 
 public class ContactPerson
 {
